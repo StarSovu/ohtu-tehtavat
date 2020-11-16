a6c4c95019b0c4362f0053539b514bba9f3d7fa1
sovu <sonja.vuorinen@helsinki.fi>
2020-11-16 20:31:06 +0200

    Selenium

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -1,16 +1,19 @@
 package ohtu;
 
+import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Tester {
 
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
+        final String URL = "http://localhost:4567";
+        driver.get(URL);
 
-        driver.get("http://localhost:4567");
         
         sleep(2);
         
@@ -24,12 +27,47 @@ public class Tester {
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
-        
+
         sleep(2);
         element.submit();
 
+        sleep(2);
+
+        element = driver.findElement(By.linkText("back to home"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+
+        Random r = new Random();
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("arto"+r.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("aaaa1111");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("aaaa1111");
+        element = driver.findElement(By.name("signup"));
+
+        sleep(2);
+        element.submit();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
         sleep(3);
-        
+
         driver.quit();
     }
     
