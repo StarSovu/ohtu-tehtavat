fb116ebf311e955a6d752df66a4558672385d279
sovu <sonja.vuorinen@helsinki.fi>
2020-11-22 16:59:13 +0200

    Mockito osa 2

--- viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -40,5 +40,21 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+    
+    @Test
+    public void kortilleLadataanRahaaJosSummaOnPositiivinen() {
+        when(kortti.getSaldo()).thenReturn(10);
+        kassa.lataa(kortti, 10);
+        
+        verify(kortti).lataa(10);
+    }
+    
+    @Test
+    public void kortilleEiLadataRahaaJosSummaOnNegatiivinen() {
+        when(kortti.getSaldo()).thenReturn(10);
+        kassa.lataa(kortti, -10);
+        
+        verify(kortti, times(0)).lataa(anyInt());
+    }
       
 }
