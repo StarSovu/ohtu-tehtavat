eb67d9cfe931c77548dc154d907f13ae8e5006ef
sovu <sonja.vuorinen@helsinki.fi>
2020-11-23 22:34:03 +0200

    Tennis refaktoroitu

--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,9 +1,11 @@
 package ohtu;
 
+import static java.lang.Math.abs;
+
 public class TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -14,67 +16,60 @@ public class TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            player1Score += 1;
         else
-            m_score2 += 1;
+            player2Score += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (player1Score==player2Score) {
+            score = draw(player1Score);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        else if (player1Score>=4 || player2Score>=4) {
+            score = scoresOverFour(player1Score, player2Score);
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        else {
+            score = scoreName(player1Score) + "-" + scoreName(player2Score);
         }
         return score;
     }
+    
+    private String scoreName(int score) {
+        if (score == 0) {
+            return "Love";
+        } else if (score == 1) {
+            return "Fifteen";
+        } else if (score == 2) {
+            return "Thirty";
+        } else if (score == 3) {
+            return "Forty";
+        } else {
+            return "";
+        }
+    }
+    
+    private String draw(int score) {
+        if (score < 4) {
+            return scoreName(score) + "-All";
+        } else {
+            return "Deuce";
+        }
+    }
+    
+    private String scoresOverFour(int score1, int score2) {
+        String score = "";
+        if (abs(score1 - score2) == 1) {
+            score = "Advantage ";
+        } else {
+            score = "Win for ";
+        }
+        if (score1 > score2) {
+            score += "player1";
+        } else {
+            score += "player2";
+        }
+        return score;
+    }
+    
 }
