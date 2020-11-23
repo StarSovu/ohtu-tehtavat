32dc9f11e6c5e9f108815f7768ee7a9fe26f584c
sovu <sonja.vuorinen@helsinki.fi>
2020-11-23 22:38:50 +0200

    hieman muokkausta

--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -8,14 +8,16 @@ public class TennisGame {
     private int player2Score = 0;
     private String player1Name;
     private String player2Name;
+    private int scoreNameChange;
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        scoreNameChange = 4;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if ("player1".equals(playerName))
             player1Score += 1;
         else
             player2Score += 1;
@@ -26,8 +28,8 @@ public class TennisGame {
         if (player1Score==player2Score) {
             score = draw(player1Score);
         }
-        else if (player1Score>=4 || player2Score>=4) {
-            score = scoresOverFour(player1Score, player2Score);
+        else if (player1Score >= scoreNameChange || player2Score >= scoreNameChange) {
+            score = scoresAfterChange(player1Score, player2Score);
         }
         else {
             score = scoreName(player1Score) + "-" + scoreName(player2Score);
@@ -50,14 +52,14 @@ public class TennisGame {
     }
     
     private String draw(int score) {
-        if (score < 4) {
+        if (score < scoreNameChange) {
             return scoreName(score) + "-All";
         } else {
             return "Deuce";
         }
     }
     
-    private String scoresOverFour(int score1, int score2) {
+    private String scoresAfterChange(int score1, int score2) {
         String score = "";
         if (abs(score1 - score2) == 1) {
             score = "Advantage ";
