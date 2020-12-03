0519c69349e56e062c539bdd6454630d0c845029
sovu <sonja.vuorinen@helsinki.fi>
2020-12-03 19:46:43 +0200

    tehtävät 2 ja 3

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -8,13 +8,18 @@ public class Main {
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
           
-        Matcher m = new And( new HasAtLeast(5, "goals"),
-                             new HasAtLeast(5, "assists"),
-                             new PlaysIn("PHI")
-        );
+        Matcher m = new And(
+            new HasAtLeast(50, "points"),
+            new Or( 
+                new PlaysIn("NYR"),
+                new PlaysIn("NYI"),
+                new PlaysIn("BOS")
+            )
+        ); 
         
         for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
+
     }
 }
